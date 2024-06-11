03b828b84c83816c8a9431aaf93553c99f6d4bd5
Kimsungyoon <k2mgroma1@snu.ac.kr>
2024-06-11 22:04:00 +0900

    Complete main.cpp

--- main.cpp
+++ main.cpp
@@ -8,12 +8,25 @@ vector<int> vec;
 
 void func(int cnt) {
 
+if(cnt==M){
+   for(int i = 0; i < M; i++){
+	cout<<vec[i]<<" ";
+   }
+   cout << endl; 
+   return;	 
+}
+int start;
+if(vec.empty()){
+start=1;
+}else{
+start=vec.back()+1;
+}
 
-
-
-
-
-
+for(int i = start; i <= N; i++){
+vec.push_back(i);
+func(cnt+1);
+vec.pop_back();
+}
 }
 
 int main() {
